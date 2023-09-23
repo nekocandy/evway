799c85bd01c70932fbf37da6f4c6874a9dbc4a2c
Piyush <bhangalepiyush@gmail.com>
2023-09-23 13:19:13 +0530

    feat: add trpc and authentication

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,29 @@
+import { UpstashRedisAdapter } from '@next-auth/upstash-redis-adapter'
+import { Redis } from '@upstash/redis'
+import GoogleProvider from 'next-auth/providers/google'
+import { NuxtAuthHandler } from '#auth'
+
+export default NuxtAuthHandler({
+  adapter: UpstashRedisAdapter(new Redis({
+    url: useRuntimeConfig().auth.upstash.URL,
+    token: useRuntimeConfig().auth.upstash.TOKEN,
+  })),
+  secret: useRuntimeConfig().auth.SECRET,
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    // @ts-expect-error vite SSR issue - need to use .default
+    GoogleProvider.default({
+      clientId: useRuntimeConfig().auth.GOOGLE_CLIENT_ID,
+      clientSecret: useRuntimeConfig().auth.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+})
