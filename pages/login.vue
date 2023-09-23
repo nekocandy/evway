4ae5cd46019989536f6b0bd15ae25c5fddccb2ed
Piyush <bhangalepiyush@gmail.com>
2023-09-23 14:28:48 +0530

    feat: complete login page design

--- pages/login.vue
+++ pages/login.vue
@@ -10,7 +10,7 @@ definePageMeta({
 </script>
 
 <template>
-  <div grid grid-cols-12 h-full border-r-8 class="border-[#E6FF01]">
+  <div class="border-[#E6FF01]" grid grid-cols-12 h-full border-r-8 font-sans text-white>
     <div
       :style="{
         background: 'linear-gradient(90deg, rgba(12, 15, 27, 0.50) 0%, rgba(255, 255, 255, 0.00) 197.9%)',
@@ -35,6 +35,31 @@ definePageMeta({
         <span class="text-[#FF24BD]">
           .
         </span>
+      </div>
+    </div>
+
+    <div col-span-8 flex flex-col items-center justify-center gap-4>
+      <div
+        flex flex-col items-center justify-center gap-8 rounded-md px-20 py-6
+        :style="{
+          background: 'linear-gradient(119deg, rgba(144, 144, 144, 0.64) 0%, rgba(35, 36, 43, 0.64) 100%)',
+          backdropFilter: 'blur(10px)',
+          border: '3px solid rgba(230, 255, 1, 0.50)',
+        }"
+      >
+        <div text-2xl font-semibold>
+          Login
+        </div>
+
+        <button flex items-center gap-2 rounded-md bg-white px-4 py-2 font-500 text-black>
+          Continue with
+          <div i-bi-google />
+        </button>
+      </div>
+
+      <div flex items-center gap-2 rounded-full bg-white px-8 py-2 text-black>
+        <span text-lg font-bold class="text-[#494C4D]">Powered By</span>
+        <img h-8 src="~/assets/redis-stack.png" alt="">
       </div>
     </div>
   </div>
