b28ac8aaea63cc43db128e668febc1bdfc79f807
Piyush <bhangalepiyush@gmail.com>
2023-09-23 13:30:40 +0530

    feat(layout): add login page layout

---
+++ layouts/login.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ScootyImage from '@/assets/scooty.png'
+</script>
+
+<template>
+  <div
+
+    h-screen bg-cover bg-center bg-no-repeat
+    :style="{
+      backgroundImage: `url(${ScootyImage})`,
+    }"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
