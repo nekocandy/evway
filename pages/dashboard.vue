66e56cb39ad550e767b40b7d0ff92cb21d077d8c
Piyush <bhangalepiyush@gmail.com>
2023-09-24 17:37:00 +0530

    fix: map not moving

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -4,7 +4,18 @@
 
 <template>
   <div grid grid-cols-12 h-full gap-4 overflow-y-hidden>
-    <div col-span-7 />
+    <div col-span-7 flex flex-col gap-6>
+      <div flex gap-12>
+        <img h-fit border-1 border-l-0 border-gray-400 rounded-xl src="https://cdn.discordapp.com/attachments/1154420365083299911/1155473574950359050/Frame_92.png">
+        <img h-fit border-1 border-l-0 border-gray-400 rounded-xl src="https://cdn.discordapp.com/attachments/1154420365083299911/1155473574715465800/Frame_91.png">
+        <img h-fit border-1 border-l-0 border-gray-400 rounded-xl src="https://cdn.discordapp.com/attachments/1154420365083299911/1155473575206195260/Frame_93.png">
+      </div>
+      <iframe flex-1 src="https://www.openstreetmap.org/export/embed.html?bbox=72.47337341308595%2C18.827016441626743%2C73.13941955566408%2C19.11921945341411&amp;layer=mapnik" style="border: 1px solid black" /><br><small><a href="https://www.openstreetmap.org/#map=12/18.9732/72.8064">View Larger Map</a></small>
+
+      <button rounded-md bg-white py-2 font-bold font-sans uppercase text-black>
+        Book EV
+      </button>
+    </div>
     <div col-span-5 flex flex-col gap-4 overflow-y-auto text-white>
       <div flex items-center justify-between>
         <h1 font-bold uppercase>
