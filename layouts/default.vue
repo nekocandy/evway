75190c3e403395ff78083a6f71827ce2ab8cc7c7
Piyush <bhangalepiyush@gmail.com>
2023-09-24 10:49:29 +0530

    feat(layout): add bg color and padding

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-  <div h-screen flex gap-2>
+  <div class="bg-[#292929]" h-screen flex gap-2 font-sans text-white>
     <Sidebar />
-    <div h-full w-full flex-1>
+    <div h-full w-full flex-1 px-4 py-4>
       <slot />
     </div>
   </div>
