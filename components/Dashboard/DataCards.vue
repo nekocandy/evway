7455471709ae190bd1e16509e9794afaf7635530
Piyush <bhangalepiyush@gmail.com>
2023-09-24 10:54:09 +0530

    feat: add vehicles search title

---
+++ components/Dashboard/DataCards.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div grid grid-cols-3>
+    <div>
+      h
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/dashboard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div grid grid-cols-12 gap-4>
+    <div col-span-7 />
+    <div col-span-5 flex flex-col gap-4 text-white>
+      <div flex items-center justify-between>
+        <h1 font-bold uppercase>
+          Vehicles Search
+        </h1>
+
+        <div flex items-center justify-center gap-2 rounded-md bg-white px-4 py-1 text-sm text-black>
+          <span>Powered By</span>
+          <img w-24 src="~/assets/redis-stack.png" alt="redis-stack">
+        </div>
+      </div>
+      <div class="bg-[#1E1E1E]" rounded-md px-4 py-4>
+        HH
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
