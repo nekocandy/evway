d8dbaaa35d1b8ecff673ab9cb6b50b7cb3b31534
Piyush <bhangalepiyush@gmail.com>
2023-09-24 11:25:08 +0530

    feat(redis/repo): create index for searching

--- lib/database/models/repo.ts
+++ lib/database/models/repo.ts
@@ -1,5 +1,11 @@
+import type { Schema } from 'redis-om'
 import { Repository } from 'redis-om'
 import type { getRedisClient } from '../redis'
 import { evModelsSchema } from './schema'
+
+export function createIndex(schema: Schema, redisClient: Awaited<ReturnType<typeof getRedisClient>>) {
+  const repo = new Repository(schema, redisClient)
+  return repo.createIndex()
+}
 
 export const getEVModelsRepository = (redisClient: Awaited<ReturnType<typeof getRedisClient>>) => new Repository(evModelsSchema, redisClient)
