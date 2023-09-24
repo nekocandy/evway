be2b1a8218a76da85fe0853b4d5d08353fcef6c0
Piyush <bhangalepiyush@gmail.com>
2023-09-24 15:53:23 +0530

    feat: profile card

---
+++ components/Profile/Card.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const { data } = useAuth()
+
+const user = data.value!.user!
+</script>
+
+<template>
+  <div class="bg-[#1E1E1E]" grid grid-cols-12 gap-4 rounded-md px-4 py-8>
+    <div col-span-8 flex flex-col gap-2>
+      {{ user.name }}
+      <div h-2 border-b-2 class="border-[#6C6C6C]" />
+      <div>
+        Age: 22
+      </div>
+      <div>
+        License Number: XXXX XX XX364
+      </div>
+      <div>
+        EV Vehicle: Ather 450X
+      </div>
+
+      <div>
+        Document Clearance: Done
+      </div>
+    </div>
+    <div col-span-4>
+      <img w-full :src="user.image || 'https://vpizza.pycz.dev/oink.svg'" rounded-md>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/profile.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div grid grid-cols-12 gap-4>
+    <div col-span-5 flex flex-col gap-4>
+      <ProfileCard />
+    </div>
+
+    <div />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
